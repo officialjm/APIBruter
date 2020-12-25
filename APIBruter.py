cbe762eb5443487f7929ed8fb885ef6afbb02866
Joseph <zombojojo181@gmail.com>
2020-12-25 10:41:01 -0500

    Update APIBruter.py

--- APIBruter.py
+++ APIBruter.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 #NAME: APIBRUTER
-#Author: Joseph Martinez
+#Author: Joseph M
 
 import requests 
 
 for api_key in range(1,100,2):
     print(f"api_key {api_key}")
-    html = requests.get(f'http://10.10.84.152:8000/api/{api_key}')
+    html = requests.get(f'http://[machine_ip]:8000/api/{api_key}')
     print(html.text) 
